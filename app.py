890f34ac0a5b0a99299b05f43b2a0757099c3a85
Wamumia <georgebrighton164@gmail.com>
2026-02-19 10:27:29 +0300

    sokogardenapi_commit

--- app.py
+++ app.py
@@ -29,10 +29,323 @@ def signup():
     return jsonify({"message":"Sign up successful                                                                                                                                                                                                                                                                                                                                                                                                                        "})
 
 
+# quioz 2 login
+@app.route("/api/signin",methods=["POST"])
+def signin():
+    email=request.form['email']
+    password=request.form['password']
+
+    print(email,password)
+# connection to db
+    connection = pymysql.connect(host="localhost",user="root", password="",database="brighton_sokogarden")
 
 
+    # create cursor to handle sql queries
+    cursor =connection.cursor(pymysql.cursors.DictCursor)
+    # create the sql query
+    sql = "select user_id,username, email,password from users where email=%s and password =%s"
+    # data to execute the query
+    data = (email,password)
+
+    # execute the query
+    cursor.execute(sql,data)
+
+    # check for resulting rows
+    if cursor.rowcount ==0:
+        return jsonify({"message": "invalid credentials"})
+    else:
+        # get user data
+        user = cursor.fetchone()
+        return jsonify({"message": "login successfull", "user": user})
+    
+
+
+# return a response
+    
 
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
