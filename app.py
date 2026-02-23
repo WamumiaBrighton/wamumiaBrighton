c9c7e7e7c17ae3147064fd975f15c77adbde22d7
Wamumia <georgebrighton164@gmail.com>
2026-02-23 10:37:20 +0300

    added get product

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import *
 import pymysql
+import os
 
 app = Flask(__name__)
-
+app.config["UPLOAD_FOLDER"] = "static/images"
 @app.route("/api/signup",methods=["POST"])
 def signup():
     username= request.form['username']
@@ -62,6 +63,60 @@ def signin():
 
 # return a response
     
+
+@app.route("/api/add_product", methods=["POST"])
+def addproduct():
+    product_name=request.form['product_name']
+    product_description=request.form['product_description']
+    product_category=request.form['product_category']
+    product_cost=request.form['product_cost']
+    product_image=request.files['product_image']
+
+
+    # getr image name
+    image_name= product_image.filename
+
+    print(product_name, product_description,product_category,product_cost,image_name                                                                                                                  )
+
+    # save the image to the folder
+    file_path= os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+    print(file_path)
+    product_image.save(file_path)
+
+    connection=pymysql.connect(host="localhost",user="root", password="",database="brighton_sokogarden")
+    # create cursor to handle the query
+    cursor= connection.cursor()
+    # create the sql query
+    sql ="insert into product_details(product_name,product_description,product_category,product_cost,product_image) values(%s,%s,%s,%s,%s)"
+    # data to execute the query
+    data=(product_name,product_description, product_category, product_cost,product_image)
+    # execute the query
+    cursor.execute(sql,data)
+    # save the data
+    connection.commit()
+
+
+    return jsonify({"message":"Done"})
+
+
+
+
+
+@app.route("/api/get_products")
+def getProducts():
+    connection = pymysql.connect(host="localhost", user="root",password="",database="brighton_sokogarden")
+    cursor= connection.cursor(pymysql.cursors.DictCursor)
+    # sql query
+    sql= "select * from product_details"
+
+    cursor.execute(sql)
+
+    if cursor.rowcount ==0:
+        return jsonify({"message":"no products found"})
+    else:
+        # fetch products
+        products = cursor.fetchall()
+        return jsonify(products)
 
 
 
